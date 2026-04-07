eb45181f729916dd9ce20a37990a643e09329a96
naresh7km <naresh7km@gmail.com>
2026-04-07 08:43:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -208,7 +208,7 @@ const ORIGIN_CONFIG = {
     audio1: "https://files-pop.s3.ap-northeast-1.amazonaws.com/aud-input.mp3",
   },
   "https://kotonohaschooljp.d3emavr1g5xutx.amplifyapp.com": {
-    srcUrl: "https://coredmc.ddhttrlb52noy.amplifyapp.com",
+    srcUrl: "https://core.d3gjoob4nilmyf.amplifyapp.com",
     audio1: "https://files-pop.s3.ap-northeast-1.amazonaws.com/aud-input.mp3",
   },
 };
